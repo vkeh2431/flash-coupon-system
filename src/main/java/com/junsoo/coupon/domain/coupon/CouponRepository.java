842fcc58fb0553e2a91d07bfbf27bd8c4b49b9ba
김준수 <vkeh2431@gmail.com>
2026-07-30 17:10:55 +0900

    feat: 내 쿠폰 목록 조회 API 추가

--- src/main/java/com/junsoo/coupon/domain/coupon/CouponRepository.java
+++ src/main/java/com/junsoo/coupon/domain/coupon/CouponRepository.java
@@ -1,9 +1,16 @@
 package com.junsoo.coupon.domain.coupon;
 
-import com.junsoo.coupon.domain.campaign.Campaign;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
     boolean existsByUserIdAndCampaignId(Long userId, Long CampaignId);
+
+    @Query("select c from Coupon c join fetch c.campaign where c.user.id = :userId and c.status = :status and c.expiresAt > now() ORDER BY c.expiresAt asc")
+    List<Coupon> findAvailableByUserId(@Param("userId") Long userId, @Param("status") Status status);
 }
